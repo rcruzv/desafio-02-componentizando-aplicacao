efed5e9f46946d054b14ec809ef9c6ccbf2d059c
Rodrigo da Cruz Vieira <rodrigocruz.vieira@portoseguro.com.br>
2022-01-07 22:20:02 -0300

    Solução desafio

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,27 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from './MovieCard';
+import { MovieProps } from "../interface/props";
+interface ContentProps {
+  movies: MovieProps[],
+  title: string
+}
+
+export function Content(props: ContentProps) {
+
+  const { movies, title} = props;
+
+  return (
+    <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {title}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {movies.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            ))}
+          </div>
+        </main>
+      </div>
+  )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,33 @@
-export function SideBar() {
-  // Complete aqui
+import '../styles/sidebar.scss';
+import { Button } from './Button';
+import { GenreProps } from '../interface/props';
+
+interface SideBarProps{
+  genres: GenreProps[],
+  id: number
+  click: Function
+}
+
+export function SideBar(props: SideBarProps) {
+
+  const { genres, id, click } = props
+  
+  return (
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => click(genre.id)}
+              selected={id === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>
+  )
 }
